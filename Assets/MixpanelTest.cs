0a934e708da36810f90bd4ee266f1404d2982602
Volodymyr Stefanyshyn <v.b.stefanyshyn@gmail.com>
2019-09-23 17:12:23 +0300

    Add test case

---
+++ Assets/MixpanelTest.cs
@@ -0,0 +1,45 @@
+﻿using mixpanel;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MixpanelTest : MonoBehaviour
+{
+    private const float Delay = 0.3f;
+
+    public TMPro.TMP_InputField input;
+
+    public void SignUp()
+    {
+        Mixpanel.Alias(input.text);
+        MakeEventDeleyed("SignUp");
+    }
+
+    public void ResetUser()
+    {
+        PlayerPrefs.DeleteAll();
+        MakeEventDeleyed("Reset");
+    }
+
+    public void Login()
+    {
+        Mixpanel.Identify(input.text);
+        MakeEventDeleyed("Login");
+    }
+
+    public void MakeEvent(string eventName)
+    {
+        Mixpanel.Track(eventName);
+    }
+
+    public void MakeEventDeleyed(string eventName)
+    {
+        StartCoroutine(WaitCoroutine(eventName));
+    }
+
+    IEnumerator WaitCoroutine(string eventName)
+    {
+        yield return new WaitForSeconds(Delay);
+        Mixpanel.Track(eventName);
+    }
+}
